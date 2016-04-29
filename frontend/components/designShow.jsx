3f4ca8917d48a2a0c5f9083314cc35532ca6c123
Guy Hadas <GuyHadas195@gmail.com>
2016-04-28 23:31:25 -0700

    styling improvements to design show

--- frontend/components/designShow.jsx
+++ frontend/components/designShow.jsx
@@ -30,13 +30,31 @@ var DesignShow = React.createClass({
       <div className="design-show">
           <div className="design-details-card">
             <div className="details-box">
-              <div className="design-username">{this.state.design.username}</div>
-              <div className="design-title">{this.state.design.title}</div>
+              <div className="details-title-username-info">
+                <div className="design-title">{this.state.design.title}</div>
+                <div>by <span className="design-username">
+                  {this.state.design.username}
+                  </span>
+                </div>
+              </div>
               <div className="design-desc">{this.state.design.description}</div>
             </div>
             <ul className="comments-list">
               <li className="comment">Wow what a dope picture dude such awesome work!</li>
               <li className="comment">I think the purple doesn't really work right here.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
+              <li className="comment">This project is really cool, but still unfinished.</li>
               <li className="comment">This project is really cool, but still unfinished.</li>
             </ul>
           </div>
