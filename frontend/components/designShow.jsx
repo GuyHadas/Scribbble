9f329ba5e8a168e168f02990ada89c84ca6a7b04
Guy Hadas <GuyHadas195@gmail.com>
2016-04-29 09:44:01 -0700

    more styling for design show page

--- frontend/components/designShow.jsx
+++ frontend/components/designShow.jsx
@@ -28,40 +28,45 @@ var DesignShow = React.createClass({
   render: function() {
     return (
       <div className="design-show">
-          <div className="design-details-card">
-            <div className="details-box">
-              <div className="details-title-username-info">
-                <div className="design-title">{this.state.design.title}</div>
-                <div>by <span className="design-username">
-                  {this.state.design.username}
-                  </span>
-                </div>
-              </div>
-              <div className="design-desc">{this.state.design.description}</div>
-            </div>
-            <ul className="comments-list">
-              <li className="comment">Wow what a dope picture dude such awesome work!</li>
-              <li className="comment">I think the purple doesn't really work right here.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-              <li className="comment">This project is really cool, but still unfinished.</li>
-            </ul>
-          </div>
 
-          <div className="design-url-show">
-            <img src={this.state.design.design_url} />
+        <div className="design-details-card">
+          <div className="details-box">
+            <div className="details-title-username-info">
+              <div className="design-title">{this.state.design.title}</div>
+              <div>by <span className="design-username">
+                {this.state.design.username}
+                </span>
+              </div>
+            </div>
+            <div className="design-desc">{this.state.design.description}</div>
           </div>
+          <ul className="comments-list">
+            <li className="comment">Wow what a dope picture dude such awesome work!</li>
+            <li className="comment">I think the purple doesn't really work right here.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">I am not really sure what you were going for I think it is nice but can be a lot better. The lighting is all off and the colors don't work with one another.</li>
+            <li className="comment">This is soooooo ugly ewww.</li>
+            <li className="comment">Yeah wow nice for sure.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+            <li className="comment">This project is really cool, but still unfinished.</li>
+          </ul>
+        </div>
+
+        <div className="design-url-show">
+          <img src={this.state.design.design_url} />
+          <div className="comment-form-box">
+            <span className="comment-form">Comment form goes here...</span>
+          </div>
+        </div>
+
       </div>
     );
   }
